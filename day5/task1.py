316066fe680578b5994b616c2498d179cbf3d6d9
Anas Haddad <hhhaaanas@gmail.com>
2023-12-15 16:20:25 +0100

    day5 - still not working :(

--- day5/task1.py
+++ day5/task1.py
@@ -5,18 +5,47 @@ headers = ["seeds", "seed-to-soil map:", "soil-to-fertilizer map:",
             "light-to-temperature map:", "temperature-to-humidity map:",
             "humidity-to-location map:"]
 
-def format_input(data: list):
-    results = {}
-    current_header = "seeds:"
-    for line in data:
-        if line in headers:
-            current_header = line
-            results[current_header] = []
-            continue
-        results[current_header].append(line)
+def get_maps(key: str, values: list):
+    if key == "seeds":
+        return key, list(map(int, re.findall(r"\d+", values[0])))
+    returns = {}
+    title = "_".join(key[:-4].split("-to-"))
+    for value in values:
+        matches = re.match(r"(\d+)\s(\d+)\s(\d+)", value)
+        for i in range(int(matches.group(3))):
+            returns[int(matches.group(2)) + i] = int(matches.group(1)) + i
+    return title, returns
 
 if __name__ == "__main__":
-    with open("data.txt", "r") as f:
-        data = f.read().splitlines()
-    print(format_input(data).keys())
+    with open("/afs/desy.de/user/h/haddadan/private/AoC/day5/data.txt", "r") as f:
+        data_iter = iter(f.read().splitlines())
+    header = "seeds"
+    data_seperated = {header: []}
+    for line in data_iter:
+        if not line:
+            header = next(data_iter)[:-1]
+            data_seperated[header] = []
+            continue
+        data_seperated[header].append(line)
+    
+    data_mapping = {}
+    for key, values in data_seperated.items():
+        new_key, new_values = get_maps(key, values)
+        data_mapping[new_key] = new_values
+    
+    locations = {}
+    seeds = data_mapping.pop("seeds")
+    for seed in seeds:
+        key_src, key_dst = "seed", "soil"
+        distanation = seed
+        while key_dst != "location":
+            distanation = data_mapping[f"{key_src}_{key_dst}"].get(distanation, distanation)
+            for key in data_mapping:
+                new_key_src, new_key_dst = key.split("_")
+                if key_dst == new_key_src:
+                    key_src, key_dst = new_key_src, new_key_dst
+                    break
+        locations[seed] = data_mapping["humidity_location"].get(distanation, distanation)
+    print(locations)
+    print(min(locations.values()))
 
